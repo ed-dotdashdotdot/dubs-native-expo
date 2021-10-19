cf022479618259c72662bde75be3a9204a59c6cd
ed-macbook-air <deeneye@yahoo.co.uk>
2021-10-19 23:10:11 +0100

    Add global stylesheet

---
+++ css/style.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from 'react-native';
+
+const globalStyles = StyleSheet.create({
+  border: {
+    borderWidth: 2,
+    borderColor: 'white',
+    borderStyle: 'solid',
+  },
+  borderRadius8: {
+    borderRadius: 8,
+  },
+  borderRadius12: {
+    borderRadius: 12,
+  },
+  fontFamilyCourier: {
+    fontFamily: 'Courier New',
+    color: 'green',
+  },
+  colorWhite: {
+    color: 'red',
+  }
+});
+
+export default globalStyles;
