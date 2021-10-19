b6a2e7842522d9acbd48bc8ef354063a622bc62f
ed-macbook-air <deeneye@yahoo.co.uk>
2021-10-20 00:19:21 +0100

    Use switch statement to show different components

--- components/Info/index.js
+++ components/Info/index.js
@@ -3,8 +3,12 @@ import { StyleSheet, Button, Text, View, ScrollView } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
 import allActions from '../../actions';
 
+import PageHeading from '../PageHeading';
 import NewGameButtons from '../shared/NewGameButtons';
 import Translate from '../Translate';
+
+import GameHome from '../GameHome';
+import GameReady from '../GameReady';
 
 // import { setGameStatus } from '../../actions';
 
@@ -35,44 +39,57 @@ const Info = () => {
       globalStyles.borderRadius12,
       styles.info,
     ]}>
-      <ScrollView>
-        <View style={styles.infoInner}>
-          <Text style={[
+      {/* <ScrollView>
+        <View style={styles.infoInner}> */}
+          {/* <Text style={[
             globalStyles.fontFamilyCourier,
             styles.infoHeading
-          ]}>Open up App.js to start working on your app!</Text>
-          <NewGameButtons />
-          {!!game.level && <Text>{game.level}</Text>}
-          {!!game.status && <Text>{game.status}</Text>}
-          {/* {!!game.status && !!game.level && <Text>BOTH</Text>} */}
-          <Text><Translate textKey='easy' /> (Translated)</Text>
-          <Text>{language.selected}</Text>
-          <Text style={[
-            globalStyles.fontFamilyCourier,
-            styles.bodyText,
-          ]}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas dui lectus, tristique eget euismod in, sagittis eu magna. Aenean lobortis arcu ac elit aliquet, dapibus scelerisque eros blandit. Praesent tincidunt, turpis non facilisis mattis, nulla ligula tincidunt ex, ac accumsan tellus magna sed risus. Fusce eget consequat mi, quis vehicula elit. Mauris volutpat mi augue, nec cursus mauris scelerisque eu. Cras tristique at nisi bibendum vulputate. Fusce id efficitur neque. Vivamus ullamcorper tortor a porta viverra.
-          </Text>
-          <Text style={[
-            globalStyles.fontFamilyCourier,
-            styles.bodyText,
-          ]}>
-            Integer dapibus eros purus, ac commodo arcu finibus nec. Morbi dui metus, sagittis a magna et, vulputate consequat ligula. Duis et tortor ut dui dapibus egestas. Phasellus hendrerit eros at laoreet dapibus. Vivamus dignissim imperdiet eros vel finibus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce lobortis facilisis ipsum, et lacinia leo laoreet eget. Maecenas ultricies hendrerit magna sit amet pellentesque.
-          </Text>
-          <Text style={[
-            globalStyles.fontFamilyCourier,
-            styles.bodyText,
-          ]}>
-            Duis magna nulla, sodales nec rutrum at, convallis non justo. Donec aliquam dictum ante, feugiat porta nibh feugiat in. Integer id tortor eleifend, convallis felis eu, hendrerit ipsum. Fusce sed aliquam erat. Duis sodales, purus ut facilisis rhoncus, massa mauris laoreet quam, eget dapibus enim tortor sit amet orci. Nunc in luctus tortor, sit amet imperdiet velit. Vestibulum dapibus eget ipsum in dapibus. Nullam lacinia diam eget quam varius, lobortis gravida ipsum accumsan. Pellentesque massa sem, dignissim eu bibendum non, sagittis sed eros. Vestibulum sed egestas ante. Curabitur vitae sagittis neque. Duis eu turpis arcu. Vestibulum nisl leo, rhoncus quis cursus at, eleifend vel libero. Cras hendrerit ipsum et felis sagittis eleifend. Donec mollis arcu porttitor, interdum eros et, cursus libero. Mauris nec libero hendrerit, mollis libero in, facilisis eros.
-          </Text>
-          <Text style={[
-            globalStyles.fontFamilyCourier,
-            styles.bodyText,
-          ]}>
-            Pellentesque tristique eleifend dapibus. Aenean semper nisi quis nulla mattis, eget imperdiet lorem euismod. In et tellus nibh. Vivamus ac quam at diam ornare hendrerit. Curabitur semper ultricies sapien ac venenatis. Curabitur posuere maximus mattis. Vivamus vehicula finibus porttitor. Morbi et nibh non odio scelerisque condimentum. Donec nec nibh eget mauris tristique faucibus ultrices eu diam. Sed quam justo, suscipit quis ultrices vitae, tincidunt vitae elit. Nulla facilisi.
-          </Text>
-        </View>
-      </ScrollView>
+          ]}>Open up App.js to start working on your app!</Text> */}
+
+          {(() => {
+            switch (game.status) {
+              // case 'image-detail':
+              //   return <ImageDetail image={imageDetail} level={level} />;
+              // case 'choose-image':
+              //   return <ChooseImage />;
+              // case 'game-over':
+              //   return <GameOver />;
+              // case 'game-paused':
+              //   return <GamePaused />;
+              case 'game-ready':
+                return (
+                  <GameReady
+                    // bossMode={bossMode}
+                    // lang={lang}
+                    // level={level}
+                    // gameImage={gameImage}
+                  />
+                );
+              // case 'what-is-boss-mode':
+              //   return (
+              //     <WhatIsBossMode
+              //       bossMode={bossMode}
+              //     />
+              //   );
+              // case 'game-on':
+              //   return (
+              //     <Game
+              //       bossMode={bossMode}
+              //       gameImage={gameImage}
+              //       invert={invert}
+              //       lang="en"
+              //       level="easy"
+              //       percentComplete={percentComplete}
+              //       status="game-on"
+              //     />
+              //   );
+              default:
+                return <GameHome />;
+            }
+          })()}
+
+        {/* </View>
+      </ScrollView> */}
     </View>
   );
 }
