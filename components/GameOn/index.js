a5b0cea33ed565a5390e0c60463ee84e33603977
ed-macbook-air <deeneye@yahoo.co.uk>
2021-10-20 18:58:23 +0100

    Add GameOn component

---
+++ components/GameOn/index.js
@@ -0,0 +1,49 @@
+import React, { useEffect } from 'react';
+import { StyleSheet, Button, Text, View, ScrollView } from 'react-native';
+import { useSelector, useDispatch } from 'react-redux';
+import allActions from '../../actions';
+
+import PageHeading from '../PageHeading';
+// import NewGameButtons from '../shared/NewGameButtons';
+import Translate from '../Translate';
+
+import Polyglot from 'node-polyglot';
+import i18n from '../../i18n';
+
+
+
+// import { setGameStatus } from '../../actions';
+
+import globalStyles from '../../css/style.js';
+import styles from './styles.js';
+
+const GameOn = props => {
+  const game = useSelector(state => state.game);
+  const language = useSelector(state => state.language);
+  const { gameActions } = { ...allActions };
+  const dispatch = useDispatch();
+
+  const polyglot = new Polyglot();
+  polyglot.extend(i18n());
+  const lang = language.selected || 'en';
+
+  const { level } = { ...props };
+
+  const whichLevelColour = level => {
+    if (level === 'easy') return 'green';
+    if (level === 'normal') return 'amber';
+    if (level === 'hard') return 'red';
+    return null;
+  }
+
+  return (
+    <ScrollView>
+      <View style={styles.infoInner}>
+        
+      </View>
+    </ScrollView>
+    
+  );
+}
+
+export default GameOn;
