d3b3d337b9403da8a150767429222d5d99578e47
ed-macbook-air <deeneye@yahoo.co.uk>
2021-10-21 23:13:48 +0100

    Add getCorners helper function

---
+++ js/helpers/getCorners/index.js
@@ -0,0 +1,38 @@
+const getCorners = (level, portrait) => {
+  if (level === 'easy') {
+    if (portrait) {
+      return {
+        bottomLeft: 20,
+        topRight: 3,
+      };
+    }
+    return {
+      bottomLeft: 18,
+      topRight: 5,
+    };
+  } else if (level === 'normal') {
+    if (portrait) {
+      return {
+        bottomLeft: 42,
+        topRight: 5,
+      };
+    }
+    return {
+      bottomLeft: 40,
+      topRight: 7,
+    };
+  } else if (level === 'hard') {
+    if (portrait) {
+      return {
+        bottomLeft: 88,
+        topRight: 7,
+      };
+    }
+    return {
+      bottomLeft: 84,
+      topRight: 11,
+    };
+  }
+};
+
+export default getCorners;
