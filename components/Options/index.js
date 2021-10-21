97c740f2209fea4c96e3c7b38770cfce6437db15
EMM18 <deeneye@yahoo.co.uk>
2021-10-21 02:43:46 +0100

    Translate button text

--- components/Options/index.js
+++ components/Options/index.js
@@ -3,12 +3,21 @@ import { Button, View, Image, TouchableOpacity, Text } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
 import allActions from '../../actions';
 
+import Polyglot from 'node-polyglot';
+import i18n from '../../i18n';
+
 import globalStyles from '../../css/style.js';
 import styles from './styles.js';
 
 const Options = () => {
+  const language = useSelector(state => state.language);
   const { gameActions } = { ...allActions };
   const dispatch = useDispatch();
+
+  const polyglot = new Polyglot();
+  polyglot.extend(i18n());
+  const lang = language.selected || 'en';
+
   return (  
     <View style={[
       globalStyles.border,
@@ -19,91 +28,13 @@ const Options = () => {
         accessibilityLabel="Back to home"
         // activeOpacity={0.5}
         color={'white'}
-        title="Back to home"
+        title={polyglot.t(`backToHome.${lang.toUpperCase()}`)}
+        // asd='backToHome'
         onPress={() => {
           dispatch(gameActions.setGameLevel(''));
           dispatch(gameActions.setGameStatus('game-home'));
         }}
-      >
-        {/* <View style={styles.options}> */}
-          <Text>Hello</Text>
-        {/* </View> */}
-      </Button>
-      {/* <View style={styles.buttonContainer}>
-        <View style={styles.de}>
-          <TouchableOpacity
-            accessibilityLabel="Switch language to German"
-            activeOpacity={0.5}
-            color={'white'}
-            title="German"
-            >
-            <Image
-              style={styles.flag}
-              source={require('../../assets/flags/de.png')}
-              />
-          </TouchableOpacity>
-        </View>
-      </View>
-      <View style={styles.buttonContainer}>
-        <View style={styles.en}>
-          <TouchableOpacity
-            accessibilityLabel="Switch language to English"
-            activeOpacity={0.5}
-            color={'white'}
-            title="English"
-            >
-            <Image
-              style={styles.flag}
-              source={require('../../assets/flags/en.png')}
-            />
-          </TouchableOpacity>
-        </View>
-      </View>
-      <View style={styles.buttonContainer}>
-        <View style={styles.es}>
-          <TouchableOpacity
-            accessibilityLabel="Switch language to Spanish"
-            activeOpacity={0.5}
-            color={'white'}
-            title="Spanish"
-          >
-            <Image
-              style={styles.flag}
-              source={require('../../assets/flags/es.png')}
-            />
-          </TouchableOpacity>
-        </View>
-      </View>
-      <View style={styles.buttonContainer}>
-        <View style={styles.it}>
-          <TouchableOpacity
-            accessibilityLabel="Switch language to Italian"
-            activeOpacity={0.5}
-            color={'white'}
-            title="Italian"
-          >
-            <Image
-              style={styles.flag}
-              source={require('../../assets/flags/it.png')}
-            />
-          </TouchableOpacity>
-        </View>
-      </View>
-      <View style={styles.buttonContainer}>
-        <View style={styles.pt}>
-          <TouchableOpacity
-            accessibilityLabel="Switch language to Portuguese"
-            activeOpacity={0.5}
-            color={'white'}
-            title="Portuguese"
-          >
-            <Image
-              style={styles.flag}
-              source={require('../../assets/flags/pt.png')}
-            />
-          </TouchableOpacity>
-        </View>
-      </View> */}
+      />
     </View>
   );
 }
