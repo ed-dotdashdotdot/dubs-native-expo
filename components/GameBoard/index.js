56e14fc6771fc09775fc6e95f7954ef68cdecff9
ed-macbook-air <deeneye@yahoo.co.uk>
2021-10-27 00:24:02 +0100

    Re-write game board with new grid system

--- components/GameBoard/index.js
+++ components/GameBoard/index.js
@@ -1,93 +1,116 @@
 import React from 'react';
 import {
-  StyleSheet,
-  Button,
-  Text,
+  // StyleSheet,
+  // Button,
+  // Text,
   View,
-  ScrollView,
+  // ScrollView,
   Image,
-  ImageBackground,
+  // ImageBackground,
   TouchableOpacity,
   useWindowDimensions
 } from 'react-native';
 
-import gameGrid from '../../js/helpers/gameGrid';
-import getCorners from '../../js/helpers/getCorners';
-import getSquares from '../../js/helpers/getSquares';
+// import gameGrid from '../../js/helpers/gameGrid';
+// import getCorners from '../../js/helpers/getCorners';
+// import getSquares from '../../js/helpers/getSquares';
+import gameArray from '../../js/helpers/gameArray';
 import gameGridPositions from '../../js/helpers/gameGridPositions';
 
 const GameBoard = props => {
   const { level } = { ...props };
-
-  let squares = getSquares(level);
-
   const { height, width } = useWindowDimensions();
   const isPortrait = () => {
     return height >= width;
   };
-  const gameHeight = height - 176 - 4;
-  const gameWidth = width - (12 * 2) - 4;
 
-  const imgSrc = require('../../assets/game-images/6x4-grid.jpg');
-  const imgWidth = isPortrait() 
-    ? gameGrid(level, gameHeight, gameWidth, isPortrait()).height
-    : gameGrid(level, gameHeight, gameWidth, isPortrait()).width
-  ;
-  const imgHeight = isPortrait() 
-    ? gameGrid(level, gameHeight, gameWidth, isPortrait()).width
-    : gameGrid(level, gameHeight, gameWidth, isPortrait()).height
-  ;
+  const gameWidth = width - 24 - 4;
+  const gameHeight = height - 128 - 24 - 24 - 4;
+
+  const gameSpecs = {
+    cols: level === 'hard' 
+      ? isPortrait() ? 8 : 12 
+      : level === 'normal' 
+        ? isPortrait() ? 6 : 8
+        : isPortrait() ? 4 : 6,
+    height: gameHeight,
+    rows: level === 'hard' 
+      ? isPortrait() ? 12 : 8 
+      : level === 'normal' 
+        ? isPortrait() ? 8 : 6
+        : isPortrait() ? 6 : 4,
+    total: level === 'hard' ? 96 : level === 'normal' ? 48 : 24,
+    width: gameWidth,
+  };
+  // let gameArray = [];
+  // for (let i=0; i < gameSpecs.total; i++) {
+  //   gameArray.push(i);
+  // }
+  // alert(gameArray(level).length);
+
+  const gameSquares = gameArray(level);
+  // gameSquares.sort(() => (Math.random() > 0.5) ? 1 : -1);
+
+  const imgSrc = require('../../assets/game-images/2-small.jpg');
 
   return (
-    <View style={{ display: 'flex' }}>
-      <View style={{ 
-        flexDirection: 'row',
+    <View 
+      style={{
+        display: 'flex',
+        flexDirection: "row",
         flexWrap: 'wrap',
-      }}>
-        {
-          squares.map((val, index) => {
-            return (
-              <View
-                key={val}
-                style={{
-                  borderBottomLeftRadius: index === getCorners(level, isPortrait()).bottomLeft && 100,
-                  borderBottomRightRadius: index === squares.length - 1 && 100,
-                  borderTopLeftRadius: index === 0 && 100,
-                  borderTopRightRadius: index === getCorners(level, isPortrait()).topRight && 100,
-                  height: gameGrid(level, gameHeight, gameWidth, isPortrait()).height,
-                  left: 0,
-                  position: 'relative',
-                  top: 0,
-                  width: gameGrid(level, gameHeight, gameWidth, isPortrait()).width,
-                }}
+        height: gameSpecs.height,
+        width: gameSpecs.width,
+      }}
+    >
+      {
+        gameSquares.map(val => {
+          return (
+            <View 
+              key={val}
+              style={{
+                height: gameSpecs.height / gameSpecs.rows,
+                overflow: 'hidden',
+                width: gameSpecs.width / gameSpecs.cols,
+              }}
+            >
+              <TouchableOpacity
+                activeOpacity={0.5}
+                // onPress={() => alert(val)}
               >
-                <TouchableOpacity
+                <Image
+                  resizeMode='stretch'
+                  source={imgSrc}
                   style={{
-                    borderColor: 'red',
-                    borderStyle: 'solid',
-                    borderWidth: 2,
+                    borderRadius: 10,
+                    height: gameSpecs.height,
+                    left: gameGridPositions(
+                      val, 
+                      level, 
+                      isPortrait(), 
+                      gameSpecs.width, 
+                      gameSpecs.height,
+                      gameSpecs.rows,
+                      gameSpecs.cols
+                      ).x,
+                    position: 'absolute',
+                    top: gameGridPositions(
+                      val, 
+                      level, 
+                      isPortrait(), 
+                      gameSpecs.width, 
+                      gameSpecs.height,
+                      gameSpecs.rows,
+                      gameSpecs.cols
+                    ).y,
+                    width: gameSpecs.width,
                   }}
-                  activeOpacity={0.75}
-                >
-                  <View style={{
-                    width: gameGrid(level, gameHeight, gameWidth, isPortrait()).width,
-                  }}>
-                    <Image
-                      resizeMode='cover'
-                      source={imgSrc}
-                      style={{
-                        left: gameGridPositions(index, level, isPortrait(), imgWidth, imgHeight).x,
-                        opacity: 0.99,
-                        top: gameGridPositions(index, level, isPortrait(), imgWidth, imgHeight).y,
-                      }} 
-                    />
-                  </View>
-                </TouchableOpacity>
-              </View>
-            );
-          })
-        }
-      </View>
+                />
+              </TouchableOpacity>
+            </View>
+          )
+        })
+      }
     </View>
   );
 }
