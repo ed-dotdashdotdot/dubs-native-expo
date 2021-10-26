2a8082590fb735dad6d780ca9e30b501229470d1
ed-macbook-air <deeneye@yahoo.co.uk>
2021-10-26 18:40:07 +0100

    Remove unused code

--- components/GameBoard/index.js
+++ components/GameBoard/index.js
@@ -18,8 +18,6 @@ import gameGridPositions from '../../js/helpers/gameGridPositions';
 
 const GameBoard = props => {
   const { level } = { ...props };
-  // alert(level);
-  // const squares = new Array(24);
 
   let squares = getSquares(level);
 
@@ -30,87 +28,21 @@ const GameBoard = props => {
   const gameHeight = height - 176 - 4;
   const gameWidth = width - (12 * 2) - 4;
 
-  // const gameGrid = (level, height, width, portrait) => {
-  //   if (portrait === false) {
-  //     return {
-  //       width: width / 6,
-  //       height: height / 4,
-  //     }  
-  //   }
-  //   return {
-  //     width: width / 4,
-  //     height: height / 6,
-  //   }
-  // };
-
-  // const borderRadius = 100;
-
-  // const imgSrc = isPortrait() 
-  //   ? require('../../assets/game-images/2.jpg') 
-  //   : require('../../assets/game-images/2.jpg')
-  // ;
   const imgSrc = require('../../assets/game-images/6x4-grid.jpg');
-  // const imgSrc = require('../../assets/game-images/2.jpg');
   const imgWidth = isPortrait() 
     ? gameGrid(level, gameHeight, gameWidth, isPortrait()).height
     : gameGrid(level, gameHeight, gameWidth, isPortrait()).width
-    // ? gameHeight
-    // : gameWidth
-    // ? 960 / 4
-    // : 960 / 4
-    // ? height
-    // : width
   ;
-  // alert(imgWidth);
   const imgHeight = isPortrait() 
     ? gameGrid(level, gameHeight, gameWidth, isPortrait()).width
     : gameGrid(level, gameHeight, gameWidth, isPortrait()).height
-    // ? gameWidth
-    // : gameHeight
-    // ? 960 / 4
-    // : 960 / 4
-    // ? height / 6
-    // : width / 6
   ;
-  // const imgWidth = isPortrait() 
-  //   ? gameWidth
-  //   : gameHeight
-  // ;
-  // const imgHeight = isPortrait() 
-  //   ? gameHeight
-  //   : gameWidth
-  // ;
-  // alert(`imgHeight: ${imgHeight} - imgWidth: ${imgWidth}`);
-  // alert(`${imgWidth} x ${imgHeight} - ${imgWidth * 6} x ${imgHeight * 4}`);
 
   return (
     <View style={{ display: 'flex' }}>
-
-      {/* <View>
-        <Image
-          source={require('../../assets/game-images/2-small.jpg')}
-        />
-      </View> */}
-      {/* <Text style={{ fontSize: 48 }}>
-        {squares.length}
-      </Text> */}
-
       <View style={{ 
-        // display: 'flex',
-        // flex: 1,
         flexDirection: 'row',
         flexWrap: 'wrap',
-        // left: 0,
-        // position: 'absolute',
-        // top: 0,
-        // marginBottom: 48,
-        // marginTop: 0,/
-        // width: '100%', 
-        // height: '100%', 
-        // overflow: 'hidden',
-        // borderWidth: 2,
-        // borderStyle: 'solid',
-        // borderColor: 'red',
       }}>
         {
           squares.map((val, index) => {
@@ -118,77 +50,35 @@ const GameBoard = props => {
               <View
                 key={val}
                 style={{
-                  // backgroundColor: `rgb(
-                  //   ${Math.floor(Math.random() * 255)}, 
-                  //   ${Math.floor(Math.random() * 255)}, 
-                  //   ${Math.floor(Math.random() * 255)}
-                  // )`,
                   borderBottomLeftRadius: index === getCorners(level, isPortrait()).bottomLeft && 100,
                   borderBottomRightRadius: index === squares.length - 1 && 100,
                   borderTopLeftRadius: index === 0 && 100,
                   borderTopRightRadius: index === getCorners(level, isPortrait()).topRight && 100,
                   height: gameGrid(level, gameHeight, gameWidth, isPortrait()).height,
-                  // height: imgHeight,
                   left: 0,
                   position: 'relative',
                   top: 0,
-                  // width: imgWidth,
                   width: gameGrid(level, gameHeight, gameWidth, isPortrait()).width,
-                  // borderWidth: 1,
-                  // borderStyle: 'solid',
-                  // borderColor: 'red',
                 }}
               >
                 <TouchableOpacity
                   style={{
-                    // fontSize: 12,
                     borderColor: 'red',
                     borderStyle: 'solid',
                     borderWidth: 2,
-                    // width: '100%',
-                    // width: imgWidth,
-                    // height: imgHeight,
-                    // height: '100%',
-                    // selfAlign: 'center',
-                    // textAlign: 'center',
-                    // display: 'flex',
                   }}
                   activeOpacity={0.75}
                 >
-                  {/* <Text>{`${val} - ${index}`}</Text> */}
                   <View style={{
-                    // width: '100%',
                     width: gameGrid(level, gameHeight, gameWidth, isPortrait()).width,
-                    // height: '100%',
-                    // overflow: 'hidden',
-                    // borderWidth: 0,
-                    // borderStyle: 'solid',
-                    // borderColor: 'red',
-                    // marginTop: 0,
-                    // marginBottom: 0,
                   }}>
                     <Image
                       resizeMode='cover'
                       source={imgSrc}
                       style={{
-                        // left: index === 1
-                        //   ? (imgWidth) * -1 
-                        //   : index === 2 
-                        //     ? ((isPortrait() ? imgWidth : imgHeight) + (isPortrait() ? imgWidth : imgHeight) * 1) * -1 
-                        //     : index === 3 
-                        //       ? ((isPortrait() ? imgWidth : imgHeight) + (isPortrait() ? imgWidth : imgHeight) * 2) * -1 
-                        //       : index === 4
-                        //         ? ((isPortrait() ? imgWidth : imgHeight) + (isPortrait() ? imgWidth : imgHeight) * 3) * -1 
-                        //         : index === 5
-                        //           ? ((isPortrait() ? imgWidth : imgHeight) + (isPortrait() ? imgWidth : imgHeight) * 4) * -1 
-                        //           : 0,
                         left: gameGridPositions(index, level, isPortrait(), imgWidth, imgHeight).x,
                         opacity: 0.99,
-                        // top: 0,
                         top: gameGridPositions(index, level, isPortrait(), imgWidth, imgHeight).y,
-                        // width: 200,
-                        // width: imgWidth,
-                        // height: imgHeight,
                       }} 
                     />
                   </View>
@@ -198,9 +88,7 @@ const GameBoard = props => {
           })
         }
       </View>
-
     </View>
-    
   );
 }
 
