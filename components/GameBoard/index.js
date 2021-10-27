632f7e9701a97860d9c568cf9079f1422bf27a9e
EMM18 <deeneye@yahoo.co.uk>
2021-10-27 14:34:25 +0100

    Put buttons onto board with background images attached

--- components/GameBoard/index.js
+++ components/GameBoard/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import {
   // StyleSheet,
   // Button,
@@ -10,15 +11,24 @@ import {
   TouchableOpacity,
   useWindowDimensions
 } from 'react-native';
+import allActions from '../../actions';
 
 // import gameGrid from '../../js/helpers/gameGrid';
 // import getCorners from '../../js/helpers/getCorners';
 // import getSquares from '../../js/helpers/getSquares';
-import gameArray from '../../js/helpers/gameArray';
-import gameGridPositions from '../../js/helpers/gameGridPositions';
+import getGameArray from '../../js/helpers/getGameArray';
+import getGameGridPositions from '../../js/helpers/getGameGridPositions';
+import getGameSpecs from '../../js/helpers/getGameSpecs';
+import sliceValue from '../../js/helpers/sliceValue';
 
 const GameBoard = props => {
   const { level } = { ...props };
+
+  const game = useSelector(state => state.game);
+  const language = useSelector(state => state.language);
+  const { gameActions } = { ...allActions };
+  const dispatch = useDispatch();
+
   const { height, width } = useWindowDimensions();
   const isPortrait = () => {
     return height >= width;
@@ -27,31 +37,70 @@ const GameBoard = props => {
   const gameWidth = width - 24 - 4;
   const gameHeight = height - 128 - 24 - 24 - 4;
 
-  const gameSpecs = {
-    cols: level === 'hard' 
-      ? isPortrait() ? 8 : 12 
-      : level === 'normal' 
-        ? isPortrait() ? 6 : 8
-        : isPortrait() ? 4 : 6,
-    height: gameHeight,
-    rows: level === 'hard' 
-      ? isPortrait() ? 12 : 8 
-      : level === 'normal' 
-        ? isPortrait() ? 8 : 6
-        : isPortrait() ? 6 : 4,
-    total: level === 'hard' ? 96 : level === 'normal' ? 48 : 24,
-    width: gameWidth,
-  };
-  // let gameArray = [];
-  // for (let i=0; i < gameSpecs.total; i++) {
-  //   gameArray.push(i);
-  // }
-  // alert(gameArray(level).length);
-
-  const gameSquares = gameArray(level);
-  // gameSquares.sort(() => (Math.random() > 0.5) ? 1 : -1);
+  const gameSpecs = getGameSpecs(level, isPortrait(), gameWidth, gameHeight);
+  let gameArray = [];
+  if (game.data.lenth === 0) {
+    const gameArray = getGameArray(level);
+    gameArray.sort(() => (Math.random() > 0.5) ? 1 : -1);
+  } else {
+    gameArray = game.data;
+  }
 
   const imgSrc = require('../../assets/game-images/2.jpg');
+
+  const makeSelection = buttonId => {
+    // console.log(`buttonId: ${buttonId}`);
+    dispatch(gameActions.setGameClicks(game.clicks + 1));
+    dispatch(gameActions.setGameHighlight([]));
+    // console.log(game);
+    console.log(`game.selected: ${game.selected}`);
+    console.log(`buttonId:      ${buttonId}`);
+    
+    if (game.selected) {
+      console.log(`YES - game.selected`);
+      if (game.selected === buttonId) {
+        console.log(`YES - game.selected === buttonId`);
+        dispatch(gameActions.setGameSelected(''));
+      } else {
+        console.log(`NOT - game.selected === buttonId`);
+        // make comparison
+        const clickedNumber = sliceValue(buttonId);
+        const selectedNumber = sliceValue(game.selected);
+        console.log(`clickedNumber: ${clickedNumber}`);
+        console.log(`selectedNumber: ${selectedNumber}`);
+        if (clickedNumber === selectedNumber) {
+          // correct selection
+          dispatch(gameActions.setGameFound([clickedNumber]));
+          dispatch(gameActions.setGameSelected(''));
+          // do not highlight if it's the last selection
+          if (
+            (game.selected !== '')
+            && ((game.found.length + 1) !== (game.data.length / 2))
+          ) {
+            dispatch(gameActions.setGameLastMove('correct'));
+            setTimeout(() => {
+              dispatch(gameActions.setGameLastMove(''));
+            }, 250);
+          }
+        } else {
+          // incorrect selection
+          dispatch(gameActions.setGameSelected(''));
+          dispatch(gameActions.setGameHighlight([game.selected, buttonId]));
+          dispatch(gameActions.setGameLastMove('incorrect'));
+          setTimeout(() => {
+            dispatch(gameActions.setGameHighlight([]));
+            dispatch(gameActions.setGameLastMove(''));
+          }, 250);
+        }
+      }
+    } else {
+      console.log(`NOT - game.selected`);
+      // add to selected
+      dispatch(gameActions.setGameSelected(buttonId));
+    }
+    console.log(game);
+    console.log(`\n`);
+  };
 
   return (
     <View 
@@ -64,19 +113,23 @@ const GameBoard = props => {
       }}
     >
       {
-        gameSquares.map(val => {
+        gameArray.map(val => {
           return (
             <View 
               key={val}
               style={{
+                // backgroundColor: 'red',
                 height: gameSpecs.height / gameSpecs.rows,
                 overflow: 'hidden',
                 width: gameSpecs.width / gameSpecs.cols,
               }}
             >
               <TouchableOpacity
-                activeOpacity={0.5}
+                activeOpacity={0.25}
                 // onPress={() => alert(val)}
+                onPress={e => {
+                  makeSelection(val);
+                }}
               >
                 <Image
                   resizeMode='stretch'
@@ -84,7 +137,7 @@ const GameBoard = props => {
                   style={{
                     borderRadius: 10,
                     height: gameSpecs.height,
-                    left: gameGridPositions(
+                    left: getGameGridPositions(
                       val, 
                       level, 
                       isPortrait(), 
@@ -94,7 +147,7 @@ const GameBoard = props => {
                       gameSpecs.cols
                       ).x,
                     position: 'absolute',
-                    top: gameGridPositions(
+                    top: getGameGridPositions(
                       val, 
                       level, 
                       isPortrait(), 
