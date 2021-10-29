bf26a941be1a5b42a9f1bd257514405192998f82
ed-macbook-air <deeneye@yahoo.co.uk>
2021-10-29 17:30:46 +0100

    Make the height and width a variable to pass down

--- components/Info/index.js
+++ components/Info/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { ScrollView, useWindowDimensions, View } from 'react-native';
-import { useSelector, useDispatch } from 'react-redux';
-import allActions from '../../actions';
+import { useWindowDimensions, View } from 'react-native';
+import { useSelector } from 'react-redux';
 import { SafeAreaInsetsContext } from 'react-native-safe-area-context';
 
 import ChooseImage from '../ChooseImage';
@@ -17,79 +16,86 @@ import globalStyles from '../../css/style.js';
 
 const Info = () => {
   const game = useSelector(state => state.game);
-  const dispatch = useDispatch();
 
   const { height, width } = useWindowDimensions();
   // const isDevicePortrait = isPortrait(height, width);
 
   return (
     <SafeAreaInsetsContext.Consumer>
-      {(insets) => (
-        <View 
-          id='infoContainer'
-          style={{
-            backgroundColor: 'purple',
-            flex: 1,
-            position: 'absolute',
-            top: 72,
-          }}
-        >
+      {(insets) => {
+        const infoHeight = height - 72 - 72 - insets.top - insets.bottom;
+        const infoWidth = width - (12 * 2);
+        return (
           <View 
-            id='info'
-            style={[
-              globalStyles.border,
-              globalStyles.borderRadius12,
-              {
-                height: height - 72 - 72 - insets.top - insets.bottom,
-                width: width - (12 * 2),
-              }
-            ]}
+            id='infoContainer'
+            style={{
+              backgroundColor: 'purple',
+              flex: 1,
+              position: 'absolute',
+              top: 72,
+            }}
           >
-            {(() => {
-              switch (game.status) {
-                // case 'image-detail':
-                //   return <ImageDetail image={imageDetail} level={level} />;
-                case 'choose-image':
-                  return <ChooseImage />;
-                case 'game-over':
-                  return <GameOver />;
-                // case 'game-paused':
-                //   return <GamePaused />;
-                case 'game-ready':
-                  return (
-                    <GameReady
-                      // bossMode={bossMode}
-                      // lang={lang}
-                      level={game.level}
-                      // gameImage={gameImage}
-                    />
-                  );
-                case 'what-is-boss-mode':
-                  return (
-                    <WhatIsBossMode
-                      bossMode={game.bossMode}
-                    />
-                  );
-                case 'game-on':
-                  return (
-                    <GameOn
-                      // bossMode={bossMode}
-                      // gameImage={gameImage}
-                      // invert={invert}
-                      // lang="en"
-                      // level="easy"
-                      // percentComplete={percentComplete}
-                      // status="game-on"
-                      level={game.level}
-                    />
-                  );
-                default:
-                  return <GameHome />;
-              }
-            })()}
+            <View 
+              id='info'
+              style={[
+                globalStyles.border,
+                globalStyles.borderRadius12,
+                {
+                  height: infoHeight,
+                  width: infoWidth,
+                }
+              ]}
+            >
+              {(() => {
+                switch (game.status) {
+                  // case 'image-detail':
+                  //   return <ImageDetail image={imageDetail} level={level} />;
+                  case 'choose-image':
+                    return <ChooseImage />;
+                  case 'game-over':
+                    return <GameOver />;
+                  // case 'game-paused':
+                  //   return <GamePaused />;
+                  case 'game-ready':
+                    return (
+                      <GameReady
+                        // bossMode={bossMode}
+                        // lang={lang}
+                        level={game.level}
+                        // gameImage={gameImage}
+                      />
+                    );
+                  case 'what-is-boss-mode':
+                    return (
+                      <WhatIsBossMode
+                        bossMode={game.bossMode}
+                      />
+                    );
+                  case 'game-on':
+                    return (
+                      <GameOn
+                        // bossMode={bossMode}
+                        // gameImage={gameImage}
+                        // invert={invert}
+                        // lang="en"
+                        // level="easy"
+                        // percentComplete={percentComplete}
+                        // status="game-on"
+                        level={game.level}
+                        dimensions={{
+                          height: infoHeight,
+                          width: infoWidth,
+                        }}
+                      />
+                    );
+                  default:
+                    return <GameHome />;
+                }
+              })()}
+            </View>
           </View>
-        </View>
-      )}
+        )
+      }}
     </SafeAreaInsetsContext.Consumer>
   );
 }
