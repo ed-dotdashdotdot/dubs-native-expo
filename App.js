4d38e5d0d6fb687cf8dfd9f37de6df531d72c0fa
EMM18 <deeneye@yahoo.co.uk>
2021-10-29 13:33:10 +0100

    Wrap content in SafeAreaView

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Button, Text, View } from 'react-native';
+import { StyleSheet, Button, Text, View, SafeAreaView } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import rootReducer from './reducers';
@@ -14,8 +14,13 @@ const App = () => {
 
   return (
     <Provider store={store}>
-      <Container />
-      <StatusBar style="auto" />
+      <SafeAreaView style={{
+        backgroundColor: 'rgba(0,0,0, 0.5)',
+        flex: 1,
+      }}>
+        <Container />
+        <StatusBar style="auto" />
+      </SafeAreaView>
     </Provider>
   );
 }
