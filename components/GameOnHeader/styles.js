d3c9ef6e8e1649cdf12ff2b6827ad3eb68f9eeb3
ed-macbook-air <deeneye@yahoo.co.uk>
2021-10-30 00:48:50 +0100

    Decrease height

--- components/GameOnHeader/styles.js
+++ components/GameOnHeader/styles.js
@@ -2,14 +2,14 @@ import { StyleSheet } from 'react-native';
 
 const gameOnHeaderStyles = StyleSheet.create({
   header: {
+    // backgroundColor: 'red',
     position: 'absolute',
     top: 0,
-    height: 48,
+    height: 24,
     top: 12,
     width: '100%',
     flex: 1,
     justifyContent: 'center',
-    // backgroundColor: 'red',
   }
 });
 
