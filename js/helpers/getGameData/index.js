8e88ba7723bcf2d2f6ace08a2404bad822a6a05d
ed-macbook-air <deeneye@yahoo.co.uk>
2021-10-31 15:25:22 +0000

    Add getGameData helper function

---
+++ js/helpers/getGameData/index.js
@@ -0,0 +1,31 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-return-assign */
+import generateRandomNumber from '../generateRandomNumber';
+import { gameArray, gameSize } from '../../../configuration/config.json';
+import sliceValue from '../sliceValue';
+
+const getGameData = level => {
+  // SHUFFLE
+  const workingArray = [...gameArray];
+  workingArray.length = gameSize[level];
+  const fullArray = [...workingArray];
+  const randomNumber = () => Math.random();
+  fullArray.sort(() => generateRandomNumber(randomNumber(), 0, 1) - 0.5); // SHUFFLES RAW NUMBERS
+  const entries = workingArray.length / 2;
+  const gameEntries = sliceValue(fullArray, entries);
+  let gameEntriesFinal = [];
+  gameEntries.map(value => gameEntriesFinal = [...gameEntriesFinal, `${value}A`, `${value}B`]);
+  gameEntriesFinal.sort(() => generateRandomNumber(randomNumber(), 0, 1) - 0.5); // SHUFFLES As AND Bs
+  return gameEntriesFinal;
+  // DO NOT SHUFFLE
+  // const workingArray = [...gameArray];
+  // workingArray.length = gameSize[level];
+  // const fullArray = [...workingArray];
+  // const entries = workingArray.length;
+  // const gameEntries = sliceValue(fullArray, entries);
+  // let gameEntriesFinal = [];
+  // gameEntries.map(value => gameEntriesFinal = [...gameEntriesFinal, `${value}B`]);
+  // return gameEntriesFinal;
+};
+
+export default getGameData;
