fad40e5b9edbff624974aefefb4468c8f856a97d
ed-macbook-air <deeneye@yahoo.co.uk>
2021-10-31 15:25:44 +0000

    Add generateRandomNumber helper function

---
+++ js/helpers/generateRandomNumber/index.js
@@ -0,0 +1,3 @@
+const generateRandomNumber = (random, min = 0, max = 1) => random * (max - min) + min;
+
+export default generateRandomNumber;
---
+++ js/helpers/generateRandomNumber/index.test.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-undef */
+import generateRandomNumber from '.';
+
+describe('generateRandomNumber', () => {
+  it.each`
+    random    | min          | max          | output
+    ${1}      | ${0}         | ${1}         | ${1}
+    ${2}      | ${0}         | ${1}         | ${2}
+    ${0.1}    | ${0}         | ${1}         | ${0.1}
+    ${0.55}   | ${0}         | ${1}         | ${0.55}
+    ${0.999}  | ${0}         | ${1}         | ${0.999}
+    ${0.999}  | ${2}         | ${3}         | ${2.999}
+    ${0.999}  | ${undefined} | ${undefined} | ${0.999}
+    `('should return $output when random is $random, min is $min and max is $max', (
+    {
+      random,
+      min,
+      max,
+      output
+    }
+  ) => {
+    expect(generateRandomNumber(random, min, max)).toBe(output);
+  });
+});
