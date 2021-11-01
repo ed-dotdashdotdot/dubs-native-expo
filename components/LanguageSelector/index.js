26298f36fbfeffb479127a3cac815ed24bc514bb
EMM18 <deeneye@yahoo.co.uk>
2021-11-01 16:29:55 +0000

    Tidy up

--- components/LanguageSelector/index.js
+++ components/LanguageSelector/index.js
@@ -15,117 +15,94 @@ const LanguageSelector = props => {
     <>
       <LanguageSelectorSelected language={language} />
       <View 
-        style={[
-          {
-            bottom: 12,
-            height: 48,
-            position: 'absolute',
-            width: '100%',
-            // backgroundColor: 'yellow',
-          },
-          // languageSelectorStyles.languageSelectorContainer
-        ]}
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          height: 48,
+          width: '100%',
+          backgroundColor: 'green',
+          position: 'absolute',
+          bottom: 12,
+        }}
       >
-        <View 
-          style={[
-            {
-              display: 'flex',
-              flexDirection: 'row',
-              height: 48,
-              width: '100%',
-            },
-            // languageSelectorStyles.languageSelector
-          ]}
-        >
-          <View style={languageSelectorStyles.buttonContainer}>
-            <View style={languageSelectorStyles.flag}>
-              <TouchableOpacity
-                accessibilityLabel="Switch language to German"
-                activeOpacity={0.5}
-                color={'white'}
-                onPress={() => {
-                  dispatch(languageActions.setChangeLanguage('de'));
-                }}
-                title="German"
-              >
-                <Image
-                  style={languageSelectorStyles.flagImage}
-                  source={require('../../assets/flags/de.png')}
-                />
-              </TouchableOpacity>
-            </View>
+        <View style={languageSelectorStyles.buttonContainer}>
+          <View style={languageSelectorStyles.flag}>
+            <TouchableOpacity
+              accessibilityLabel="Switch language to German"
+              activeOpacity={0.5}
+              onPress={() => {
+                dispatch(languageActions.setChangeLanguage('de'));
+              }}
+            >
+              <Image
+                source={require('../../assets/flags/de.png')}
+                style={languageSelectorStyles.flagImage}
+              />
+            </TouchableOpacity>
           </View>
-          <View style={languageSelectorStyles.buttonContainer}>
-            <View style={languageSelectorStyles.flag}>
-              <TouchableOpacity
-                accessibilityLabel="Switch language to English"
-                activeOpacity={0.5}
-                color={'white'}
-                onPress={() => {
-                  dispatch(languageActions.setChangeLanguage('en'));
-                }}
-                title="English"
-              >
-                <Image
-                  style={languageSelectorStyles.flagImage}
-                  source={require('../../assets/flags/en.png')}
-                />
-              </TouchableOpacity>
-            </View>
+        </View>
+        <View style={languageSelectorStyles.buttonContainer}>
+          <View style={languageSelectorStyles.flag}>
+            <TouchableOpacity
+              accessibilityLabel="Switch language to English"
+              activeOpacity={0.5}
+              onPress={() => {
+                dispatch(languageActions.setChangeLanguage('en'));
+              }}
+            >
+              <Image
+                source={require('../../assets/flags/en.png')}
+                style={languageSelectorStyles.flagImage}
+              />
+            </TouchableOpacity>
           </View>
-          <View style={languageSelectorStyles.buttonContainer}>
-            <View style={languageSelectorStyles.flag}>
-              <TouchableOpacity
-                accessibilityLabel="Switch language to Spanish"
-                activeOpacity={0.5}
-                color={'white'}
-                onPress={() => {
-                  dispatch(languageActions.setChangeLanguage('es'));
-                }}
-                title="Spanish"
-              >
-                <Image
-                  style={languageSelectorStyles.flagImage}
-                  source={require('../../assets/flags/es.png')}
-                />
-              </TouchableOpacity>
-            </View>
+        </View>
+        <View style={languageSelectorStyles.buttonContainer}>
+          <View style={languageSelectorStyles.flag}>
+            <TouchableOpacity
+              accessibilityLabel="Switch language to Spanish"
+              activeOpacity={0.5}
+              onPress={() => {
+                dispatch(languageActions.setChangeLanguage('es'));
+              }}
+            >
+              <Image
+                source={require('../../assets/flags/es.png')}
+                style={languageSelectorStyles.flagImage}
+              />
+            </TouchableOpacity>
           </View>
-          <View style={languageSelectorStyles.buttonContainer}>
-            <View style={languageSelectorStyles.flag}>
-              <TouchableOpacity
-                accessibilityLabel="Switch language to Italian"
-                activeOpacity={0.5}
-                color={'white'}
-                onPress={() => {
-                  dispatch(languageActions.setChangeLanguage('it'));
-                }}
-                title="Italian"
-              >
-                <Image
-                  source={require('../../assets/flags/it.png')}
-                  style={languageSelectorStyles.flagImage}
-                />
-              </TouchableOpacity>
-            </View>
+        </View>
+        <View style={languageSelectorStyles.buttonContainer}>
+          <View style={languageSelectorStyles.flag}>
+            <TouchableOpacity
+              accessibilityLabel="Switch language to Italian"
+              activeOpacity={0.5}
+              onPress={() => {
+                dispatch(languageActions.setChangeLanguage('it'));
+              }}
+            >
+              <Image
+                source={require('../../assets/flags/it.png')}
+                style={languageSelectorStyles.flagImage}
+              />
+            </TouchableOpacity>
           </View>
-          <View style={languageSelectorStyles.buttonContainer}>
-            <View style={languageSelectorStyles.flag}>
-              <TouchableOpacity
-                accessibilityLabel="Switch language to Portuguese"
-                activeOpacity={0.5}
-                color={'white'}
-                onPress={() => {
-                  dispatch(languageActions.setChangeLanguage('pt'));
-                }}
-                title="Portuguese"
-              >
-                <Image
-                  style={languageSelectorStyles.flagImage}
-                  source={require('../../assets/flags/pt.png')}
-                />
-              </TouchableOpacity>
-            </View>
+        </View>
+        <View style={languageSelectorStyles.buttonContainer}>
+          <View style={languageSelectorStyles.flag}>
+            <TouchableOpacity
+              accessibilityLabel="Switch language to Portuguese"
+              activeOpacity={0.5}
+              onPress={() => {
+                dispatch(languageActions.setChangeLanguage('pt'));
+              }}
+            >
+              <Image
+                source={require('../../assets/flags/pt.png')}
+                style={languageSelectorStyles.flagImage}
+              />
+            </TouchableOpacity>
           </View>
         </View>
       </View>
--- components/LanguageSelector/styles.js
+++ components/LanguageSelector/styles.js
@@ -1,34 +1,15 @@
 import { StyleSheet } from 'react-native';
 
 const languageSelectorStyles = StyleSheet.create({
-  // languageSelectorContainer: {
-  //   bottom: 12,
-  //   height: 48,
-  //   position: 'absolute',
-  //   width: '100%',
-  // },
-  // languageSelector: {
-  //   display: 'flex',
-  //   flexDirection: 'row',
-  //   height: 48,
-  //   width: '100%',
-  // },
   buttonContainer: {
     alignItems: 'center',
     flex: 1,
-    justifyContent: 'center',
-    // borderColor: 'yellow',
-    // borderStyle: 'solid',
-    // borderWidth: 1,
   },
   flag: {
-    aspectRatio: 1/1,
-    backgroundColor: 'black',
     borderColor: 'white',
     borderRadius: 24,
     borderStyle: 'solid',
     borderWidth: 1,
-    flex: 1,
   },
   flagImage: {
     height: 46,
