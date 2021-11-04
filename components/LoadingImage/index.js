75120c268570dd954d401887db35a04191a07887
ed-macbook-air <deeneye@yahoo.co.uk>
2021-11-04 00:55:29 +0000

    Use ExpoFastImage - Dispatches on image load - Randomise image if nothing is selected - Displays 'loading...' text

--- components/LoadingImage/index.js
+++ components/LoadingImage/index.js
@@ -12,20 +12,27 @@ import allActions from '../../actions';
 // import FastImage from 'react-native-fast-image';
 import ExpoFastImage from 'expo-fast-image';
 
+import { defaultImages } from '../../configuration/config.json';
 import { ipAddress } from '../../configuration/config.json';
 
 const LoadingImage = props => {
-  const { imageToUse } = { ...props };
-  console.log(imageToUse)
+  let { imageToUse } = { ...props };
+
+  // console.log(imageToUse);
+  // console.log(typeof imageToUse);
+  // console.log(`--${imageToUse}--`);
+  if (imageToUse == '') {
+    // console.log('HERE');
+    imageToUse = defaultImages[Math.floor(Math.random() * defaultImages.length)];
+  }
+  // console.log(imageToUse);
+  // console.log(typeof imageToUse);
+  // console.log(`--${imageToUse}--`);
+
   const game = useSelector(state => state.game);
   const images = useSelector(state => state.images);
   const { gameActions, imagesActions } = { ...allActions };
   const dispatch = useDispatch();
-
-  useEffect(() => {
-    // console.log('LOADIMAGE - USEEFFECT');
-    // dispatch(imagesActions.setImagesLoaded(true));
-  }, []);
 
   const imageLoaded = () => {
     // this.setState(() => ({ loaded: true }))
@@ -33,31 +40,115 @@ const LoadingImage = props => {
     // console.log(images.loaded);
     // console.log('images:');
     // console.log(images);
-    // console.log('imageLoaded');
+    console.log('imageLoaded');
     return () => true;
   };
 
-  console.log(`images.loaded: ${images.loaded}`);
-  const imageSrc = `http://${ipAddress.server}/dubs-cdn/image/?image=${imageToUse}&size=large&qwe=123`;
-  console.log(`imageSrc: ${imageSrc}`);
+  // useEffect(() => {
+  //   // console.log('===== ===== ===== ===== =====')
+  //   // console.log(`imageToUse1: ---${imageToUse}--- goes here`);
+  //   // console.log('LOADIMAGE - USEEFFECT');
+  //   // dispatch(imagesActions.setImagesLoaded(true));
+  //   // if null then choose random image
+  //   if (imageToUse == '') {
+  //     // console.log('HERE');
+  //     imageToUse = defaultImages[Math.floor(Math.random() * defaultImages.length)];
+  //   } else {
+  //     // console.log('There')
+  //   }
+  //   // console.log('defaultImages:')
+  //   // console.log(defaultImages)
+  //   // console.log(`imageToUse2: ---${imageToUse}--- goes here`);
+  //   // console.log('-----');
+    
+  //   // return imageToUse = imageToUse => imageToUse;  
+  //   // return whichImage = () => imageSrc;
+    
+  // }, []);
+  
+  // const imageSrc = {
+  //   uri: `http://${ipAddress.server}/dubs-cdn/image/?image=${imageToUse}&size=large&qwe=123`,
+  //   cache: 'force-cache'
+  // };
+  const imageSrc = `http://${ipAddress.server}/dubs-cdn/image/?image=${imageToUse}.jpg&size=large`;
+  // const imageSrc = `http://${ipAddress.server}/dubs-cdn/img/large/${imageToUse}.jpg`;
+  // console.log(`images.loaded: ${images.loaded}`);
+  
+  // console.log(`imageSrc:`)
+  // console.log(imageSrc);
+  // console.log('imageToUse:');
+  // console.log(imageToUse);
+  // console.log(`whichImage:`)
+  // console.log(whichImage());
 
   return (
-    <ExpoFastImage
-      // onLoad={imageLoaded} 
-      onLoad={() => {
-        console.log('onLoad');
-        dispatch(imagesActions.setImagesLoaded(true));
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
       }}
-      // resizeMode={'contain'}
-      // source={{uri: `http://${ipAddress.server}/dubs-cdn/image/?image=${images.selected}`}}
-      // source={{
-      //   uri: imageSrc,
-      //   // headers: { Authorization: 'someAuthToken' },
-      //   // priority: FastImage.priority.normal,
-      // }}
-      uri={imageSrc}
-      cacheKey={`image-${images.selected}`}
-    />
+    >
+      <Text
+        style={{
+          color: 'white',
+          fontSize: 24,
+          textAlign: 'center',
+        }}  
+      >
+        Loading...
+      </Text>
+      <ExpoFastImage
+        // onLoad={imageLoaded} 
+        // onLoad={() => {
+        //   console.log('onLoad');
+        //   // dispatch(imagesActions.setImagesLoaded(true));
+        // }}
+
+        // onLoadStart={() => {
+        //   // console.log("onLoadStart here");
+        // }}
+        onLoad={() => {
+          // console.log("onLoad here");
+          dispatch(gameActions.setGameStatus('game-on'));
+          dispatch(imagesActions.setImagesLoaded(true));
+          dispatch(imagesActions.setImagesSelected(imageToUse));
+          // start timer
+        }}
+        // onLoadEnd={() => {
+        //   // console.log("onLoadEnd here");
+        // }}
+        // defaultSource={NoProfile}
+        //  loadingIndicatorSource={require("@images/profile_placeholder.png")}
+
+
+        // resizeMode={'contain'}
+        // source={{uri: `http://${ipAddress.server}/dubs-cdn/image/?image=${images.selected}`}}
+        // source={{
+        //   uri: imageSrc,
+        //   // headers: { Authorization: 'someAuthToken' },
+        //   // priority: FastImage.priority.normal,
+        // }}
+        uri={imageSrc}
+        cacheKey={`image-${imageToUse}-large`}
+        style={{
+          backgroundColor: 'red',
+          borderColor: 'white',
+          // borderStyle: 'solid',
+          borderWidth: 2,
+          height: 10,
+          width: 10,
+          opacity: 0,
+        }}
+      />
+      {/* <Text
+        style={{
+          color: 'white',
+          fontSize: 50,
+        }}  
+      >
+        {imageSrc}
+      </Text> */}
+    </View>
   );
 }
 
