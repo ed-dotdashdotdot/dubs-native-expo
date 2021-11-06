ea68977a68ca1c1d61e332fb3db902878e6a25e6
ed-macbook-air <deeneye@yahoo.co.uk>
2021-11-06 14:27:46 +0000

    Tidy up

--- components/AppStateManager/index.js
+++ components/AppStateManager/index.js
@@ -1,20 +1,14 @@
 import { useEffect } from 'react';
 import { AppState } from 'react-native';
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import allActions from '../../actions';
 
 const AppStateManager = props => {
   const { status, children  } = { ...props };
-  const game = useSelector(state => state.game);
   const { gameActions } = { ...allActions };
   const dispatch = useDispatch();
-  const handleAppStateChange = (state, gameStatus=game.status) => {
-    // console.log`gameStatus: ${gameStatus}`);
-    console.log(`status: ${status}`);
-    // console.log(game);
-    // console.log('- - -');
-    if(game.status === 'game-on' || status === 'game-on' || gameStatus === 'game-on') {
-      console.log("GAME ON");
+  const handleAppStateChange = state => {
+    if (status === 'game-on') {
       if (state !== 'active') {
         dispatch(gameActions.setGameStatus('game-paused'));
       }
