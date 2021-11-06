b950ea86f23f6a364327e30365f24b457fa7d5e8
ed-macbook-air <deeneye@yahoo.co.uk>
2021-11-06 20:58:52 +0000

    Create SVG component for header logo

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,40 +1,20 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
 
-import globalStyles from '../../css/style.js';
-import styles from './styles.js';
+import LogoSvg from './components/LogoSvg';
 
 const Header = () => {
-  const sharedCss = [
-    globalStyles.colorWhite,
-    globalStyles.fontFamilyCourier,
-  ];
   return (
-    <View style={[
-      styles.headerContainer,
-    ]}>
-      <View style={[
-        globalStyles.border,
-        globalStyles.borderRadius12,
-        styles.header,
-      ]}>
-        <Text style={[
-          sharedCss,
-          styles.headerD
-        ]}>D</Text>
-        <Text style={[
-          sharedCss,
-          styles.headerU
-        ]}>U</Text>
-        <Text style={[
-          sharedCss,
-          styles.headerB
-        ]}>B</Text>
-        <Text style={[
-          sharedCss,
-          styles.headerS
-        ]}>S</Text>
-      </View>
+    <View
+      style={{
+        height: 48,
+        position: 'absolute',
+        top: 12,
+        width: 48 * 4,
+        zIndex: 100,
+      }}
+    >
+      <LogoSvg />
     </View>
   );
 }
