d9e96e0708d2296b700ff77d3cad75b48a6c5f44
ed-macbook-air <deeneye@yahoo.co.uk>
2021-11-06 22:55:53 +0000

    Tidy up

--- css/style.js
+++ css/style.js
@@ -11,30 +11,6 @@ const globalStyles = StyleSheet.create({
   borderRadius8: {
     borderRadius: 8,
   },
-  // borderRadius12: {
-  //   borderRadius: 12,
-  // },
-  // fontFamilyCourier: {
-  //   fontFamily: 'Courier New',
-  // },
-  // colorWhite: {
-  //   color: colours.white,
-  // },
-  // padding8: {
-  //   padding: 8,
-  // },
-  // padding12: {
-  //   padding: 12,
-  // },
-  // backgroundAmber: {
-  //   backgroundColor: '#ff9900',
-  // },
-  // backgroundGreen: {
-  //   backgroundColor: '#00aa00',
-  // },
-  // backgroundRed: {
-  //   backgroundColor: '#aa0000',
-  // },
   infoInner: {
     paddingBottom: 0,
     paddingLeft: 12,
