34a632172ba642af9ff94df720a32c76c9a6c6ab
EMM18 <deeneye@yahoo.co.uk>
2021-11-07 00:43:35 +0000

    Tidy up

--- components/GameOn/index.js
+++ components/GameOn/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import { View } from 'react-native';
+// import { View } from 'react-native';
 
 import GameBoard from '../GameBoard';
 
 const GameOn = props => {
   const { dimensions, level } = { ...props };
   return (
-    <View>
-      <GameBoard
-        dimensions={dimensions}
-        level={level}
-      />
-    </View>
+    <GameBoard
+      dimensions={dimensions}
+      level={level}
+    />
   );
 }
 
