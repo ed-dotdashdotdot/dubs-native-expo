3641304548e7ebba8fd337c3c4501fab65e87bf9
ed-macbook-air <deeneye@yahoo.co.uk>
2021-11-09 11:57:28 +0000

    Remove un-needed View

--- components/ChooseImage/index.js
+++ components/ChooseImage/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ScrollView, View } from 'react-native';
+import { ScrollView } from 'react-native';
 import PropTypes from 'prop-types';
 
 import { useSelector } from 'react-redux';
@@ -17,15 +17,13 @@ const ChooseImage = props => {
         <PageHeading 
           textKey={'chooseAnImageToPlayWith'}
         />
-        <View>
-          {images.sections.map(val => (
-            <DrawSection
-              dimensions={dimensions}
-              key={val.name} 
-              section={val.name} 
-            />
-          ))}
-        </View>
+        {images.sections.map(val => (
+          <DrawSection
+            dimensions={dimensions}
+            key={val.name} 
+            section={val.name} 
+          />
+        ))}
       </InfoInner>
     </ScrollView>
   );
