1d49b903b1a79f77c67dc136faef646485cfdd8f
EMM18 <deeneye@yahoo.co.uk>
2021-11-15 01:07:48 +0000

    Remove android snapshots from config

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
   projects: [
     // Skipping Node because we want to test DOM presets only
     withEnzyme(require('jest-expo/ios/jest-preset')),
-    withEnzyme(require('jest-expo/android/jest-preset')),
+    // withEnzyme(require('jest-expo/android/jest-preset')),
     // The Enzyme support added to web is different from that added to native, which `withEnzyme` handles
     // Luckily you won't have to do anything special because it reads the platform from
     // `haste.defaultPlatform` of the provided Jest config
