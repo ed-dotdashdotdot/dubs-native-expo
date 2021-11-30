c311a0ed6715de7329129db2490df5ef4915591a
ed-macbook-air <deeneye@yahoo.co.uk>
2021-11-30 16:34:13 +0000

    Translate the alert for high score deletion

--- components/buttons/DeleteHighScoreButton/index.js
+++ components/buttons/DeleteHighScoreButton/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import { TouchableOpacity, View } from 'react-native';
+import { Alert, TouchableOpacity, View } from 'react-native';
 import PropTypes from 'prop-types';
+
+import Polyglot from 'node-polyglot';
+import i18n from '../../../i18n';
 
 import { useSelector, useDispatch } from 'react-redux';
 import allActions from '../../../actions';
@@ -8,17 +11,40 @@ import allActions from '../../../actions';
 import DeleteHighScoreButtonSvg from './components/DeleteHighScoreButtonSvg';
 
 const DeleteHighScoreButton = ({ entryKey, width }) => {
-  const { duration } = useSelector(state => state);
+  const { duration, language } = useSelector(state => state);
   const { durationActions } = { ...allActions };
   const dispatch = useDispatch();
+  const polyglot = new Polyglot();
+  polyglot.extend(i18n());
+  const lang = language.selected || 'en-GB';
   const highScoresFiltered = duration.highScores.filter(val => {
     return val.indexOf(entryKey) === -1;
   });
+
+  const createTwoButtonAlert = () => {
+    Alert.alert(
+      polyglot.t(`deleteThisHighScore.${lang}`),
+      null, [
+        {
+          text: polyglot.t(`cancel.${lang}`),
+          style: 'cancel'
+        },
+        { 
+          text: polyglot.t(`okay.${lang}`), 
+          onPress: () => {
+            dispatch(durationActions.setDurationHighScores(highScoresFiltered));
+          }
+        }
+      ]
+    );
+  };
+
   return (
     <TouchableOpacity
-      onPress={() => {
-        dispatch(durationActions.setDurationHighScores(highScoresFiltered));
-      }}
+      // onPress={() => {
+      //   dispatch(durationActions.setDurationHighScores(highScoresFiltered));
+      // }}
+      onPress={createTwoButtonAlert}
     >
       <View
         style={{
